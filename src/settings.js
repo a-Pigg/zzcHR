bb19c10a311649d1280c19bbfa353a8006f5a097
zc <2535494488@qq.com>
2025-08-25 15:51:52 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // 设置网站标题
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 固定头部
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo
+  sidebarLogo: true
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- 设置 icon-class=图标名字（没有后缀） -->
+    <svg-icon icon-class="dashboard" class-name="dashboard-icon" />
+    <svg-icon icon-class="eye"></svg-icon>
+    <svg-icon icon-class="skill"></svg-icon>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
